23b2888511f756364b1b439b8e00bdbf1133fc59
Petter Berven <petter.berven@nhn.no>
2024-08-22 21:06:29 +0200

    dirty fix to have XDG_RUNTIME_DIR for ssh-agent on macos

--- zsh/ssh-agent.sh
+++ zsh/ssh-agent.sh
@@ -1,7 +1,15 @@
-#!/usr/bin/zsh
+#!/usr/bin/env zsh
+
+if [[ -z $XDG_RUNTIME_DIR ]]; then
+    XDG_RUNTIME_DIR="$HOME/.local/XDG_RUNTIME_DIR"
+fi
+
+if [[ ! -d $XDG_RUNTIME_DIR ]]; then
+    mkdir -p $XDG_RUNTIME_DIR
+fi
 
 if ! pgrep -u "$USER" ssh-agent >/dev/null; then
-  ssh-agent -t 1h >"$XDG_RUNTIME_DIR/ssh-agent.env"
+  ssh-agent -t 1h > "$XDG_RUNTIME_DIR/ssh-agent.env"
 fi
 
 if [[ ! -f "$SSH_AUTH_SOCK" ]]; then
